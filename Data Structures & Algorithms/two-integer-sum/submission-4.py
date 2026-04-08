ef970d08c11f8f8f8d8af9c356ac170d9852834b
Pratyush Dev <99004602+prtdv@users.noreply.github.com>
2026-04-08 18:44:49 +0530

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashm={} #storing (value,index) for comparison in one pass.
+        for i in range(0,len(nums)):
+            value=target-nums[i]
+            if value in hashm:
+                return [hashm[value],i]
+            else:
+                hashm[nums[i]]=i
+        return False
