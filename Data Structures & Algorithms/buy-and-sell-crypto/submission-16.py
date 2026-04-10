3db1638f88cee782da2c209cb67f4a8789ae4902
Pratyush Dev <99004602+prtdv@users.noreply.github.com>
2026-04-10 18:26:04 +0530

    Add: buy-and-sell-crypto - submission-16

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-16.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices):
+        l = 0
+        res = 0
+
+        for r in range(1,len(prices)):
+            
+            # while window is invalid
+            while l < r and prices[l] > prices[r]:
+                l += 1   # shrink window
+
+            # now window is valid
+            res = max(res, prices[r] - prices[l])
+
+        return res
