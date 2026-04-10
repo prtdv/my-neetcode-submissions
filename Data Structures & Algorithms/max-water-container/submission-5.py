b715283a4f0d135991b5c6393eab3527a478a8b2
Pratyush Dev <99004602+prtdv@users.noreply.github.com>
2026-04-10 12:00:24 +0530

    Add: max-water-container - submission-5

---
+++ Data Structures & Algorithms/max-water-container/submission-5.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        #take width in consideration (r-l)
+        #take min height of l r's heights.
+
+        def areaf(h1,h2,i1,i2):
+            area=min(h1,h2)*abs(i1-i2)
+            return area
+        
+        maxWeight=0
+        l=0
+        r=len(heights)-1
+
+        while l<r:
+            x=areaf(heights[l],heights[r],l,r)
+            maxWeight=max(x,maxWeight)
+            
+            if heights[l]>heights[r]:
+                r-=1
+            else:
+                l+=1
+        
+        return maxWeight
+
+            
+
+
