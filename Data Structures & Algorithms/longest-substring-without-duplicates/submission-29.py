5263f8aa799531a55f38214649586f3e76d765c2
Pratyush Dev <99004602+prtdv@users.noreply.github.com>
2026-04-11 16:09:38 +0530

    Update: longest-substring-without-duplicates - submission-29

--- Data Structures & Algorithms/longest-substring-without-duplicates/submission-29.py
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-29.py
@@ -3,7 +3,7 @@ class Solution:
         window=set()
         l=0
         maxc=0
-        for r in range(0, len(s)):
+        for r in range(len(s)):
             while l<r and s[r] in window:
                 window.remove(s[l]) #always remove from the left side, shrink from left.
                 l+=1
