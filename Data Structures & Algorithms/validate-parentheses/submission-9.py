468dd9935e68caa4bcaba3ee60170452b226e920
Pratyush Dev <99004602+prtdv@users.noreply.github.com>
2026-08-28 11:25:11 -0400

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        closedict={
+            '}':'{',
+            ']':'[',
+            ')':'('
+        }
+        stack=[]
+        for idx,i in enumerate(s):
+            if i in closedict.values():
+                stack.append(i)
+            elif i in closedict.keys() and len(stack)!=0 and stack[-1]==closedict[i]:
+                stack.pop()
+            else:
+                stack.append(i)
+
+        if len(stack)==0:
+            return True
+        else:
+            return False
